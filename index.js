3045c4678674de8917931658560fcaf581bfe5e8
Melchionr <carlosquitumba582@gmail.com>
2026-06-07 01:15:36 +0100

    quaz

--- index.js
+++ index.js
@@ -23,32 +23,59 @@ app.get("/",function(req,res){
 });
 
 //rota de precos, para a unca pagina dinamica. a unica dinamica
-app.get("/preco",async function(req,res){
-    const data =await prisma.price.create({
-        data:req.body
-    });
-    console.log(data)
-    res.render("preco",{
-        propina:data.propina,
-        cartao:data.cartao,
-        uniforme_puniv:data.uniforme_puniv,
-        uniforme_enfermagem:data.uniforme_enfermagem,
-        manual:data.manual,
-        matricula:data.matricula
-    },
-);
-})
+
 
 //rota para aceder ao painel preciso a proteger
-app.get("/novela",function(req,res){
-    res.sendFile(__dirname+"/login.html");
+app.get("/novela", function(req, res) {
+    res.sendFile(__dirname + "/login.html");
 });
 
 
 
-app.post("/post/price", async function(req,res){
-    const response = await prisma.price.findMany()
-res.json({response})
+app.post("/post/price", async function(req, res) {
+    try {
+
+        const data = await prisma.price.update({
+            where: {
+                id: 1 // ou o id fixo do teu sistema
+            },
+            data: {
+                matricula: req.body.matricula,
+                propina: req.body.propina,
+                manual: req.body.manual,
+                cartao: req.body.cartao,
+                uniforme_puniv: req.body.uniforme_puniv,
+                uniforme_enfermagem: req.body.uniforme_enfermagem
+            }
+        });
+
+        res.json({
+            message: "Atualizado com sucesso",
+            data
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+app.get("/preco", async function(req,res){
+
+    const data = await prisma.price.findFirst({where:{id:1}});
+
+    res.render("preco", {
+        propina: data?.propina,
+        cartao: data?.cartao,
+        uniforme_puniv: data?.uniforme_puniv,
+        uniforme_enfermagem: data?.uniforme_enfermagem,
+        manual: data?.manual,
+        matricula: data?.matricula
+    },);
+
+});
+app.get("/all", async function(res,res){
+    const response = await prisma.price.findMany();
+    res.json({response})
 })
 // /Listen do serer/
 app.listen(3000,function(){
